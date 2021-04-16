7a135e06fd499ceef4b903ee1b620825fb6b1dfb
eduuh <edwinkamaumuraya0@outlook.com>
2021-04-16 20:08:16 +0300

    feat: created a new project.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace acr_cicd_dotnetcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
