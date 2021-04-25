0b52b54834b63a808990fa81596137a215c0672d
eduuh <edwinkamaumuraya0@outlook.com>
2021-04-25 21:19:29 +0300

    feat: Upddated errorViweModel

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace acr_cicd_dotnetcore.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public DateTime DateOccurred { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
